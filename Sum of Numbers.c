ce2af0ee0191cb63734b0dc1709ec4f68d8a4e33
Raiyan Bin Karim <raiyan772ab0@gmail.com>
2023-10-31 22:00:52 +0600

    first c problem

---
+++ Sum of Numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    scanf("%d",&n);
+    int A[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&A[i]);
+        sum+=A[i];
+    }
+    printf("%d\n",sum);
+
+}
